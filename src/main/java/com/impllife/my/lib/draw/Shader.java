3865037e82bbe8f397ad9a8b8031c7516f1009bf
ImplementLife <lmplementlifeg@gmail.com>
2022-10-17 02:54:53 +0300

    Refactoring (naming in Shader and Texture)

--- src/main/java/com/impllife/my/lib/draw/Shader.java
+++ src/main/java/com/impllife/my/lib/draw/Shader.java
@@ -8,11 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.FloatBuffer;
 
 import static org.lwjgl.opengl.GL20.*;
@@ -20,66 +17,66 @@ import static org.lwjgl.opengl.GL20.*;
 public class Shader implements Disposable {
 	private static final Logger LOG = LoggerFactory.getLogger(Shader.class);
 
-	private int programObject;
-	private int vertexShaderObject;
-	private int fragmentShaderObject;
+	private int programId;
+	private int vertexShaderId;
+	private int fragmentShaderId;
 	
 	Shader(String filename) {
-		programObject = glCreateProgram();
+		programId = glCreateProgram();
 		
-		vertexShaderObject = glCreateShader(GL_VERTEX_SHADER);
-		glShaderSource(vertexShaderObject, readFile(filename + ".vs"));
-		glCompileShader(vertexShaderObject);
-		if (glGetShaderi(vertexShaderObject, GL_COMPILE_STATUS) != 1) {
-			LOG.error(glGetShaderInfoLog(vertexShaderObject));
+		vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexShaderId, readFile(filename + ".vs"));
+		glCompileShader(vertexShaderId);
+		if (glGetShaderi(vertexShaderId, GL_COMPILE_STATUS) != 1) {
+			LOG.error(glGetShaderInfoLog(vertexShaderId));
 			throw new IllegalStateException();
 		}
 		
-		fragmentShaderObject = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fragmentShaderObject, readFile(filename + ".fs"));
-		glCompileShader(fragmentShaderObject);
-		if (glGetShaderi(fragmentShaderObject, GL_COMPILE_STATUS) != 1) {
-			LOG.error(glGetShaderInfoLog(fragmentShaderObject));
+		fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragmentShaderId, readFile(filename + ".fs"));
+		glCompileShader(fragmentShaderId);
+		if (glGetShaderi(fragmentShaderId, GL_COMPILE_STATUS) != 1) {
+			LOG.error(glGetShaderInfoLog(fragmentShaderId));
 			throw new IllegalStateException();
 		}
 		
-		glAttachShader(programObject, vertexShaderObject);
-		glAttachShader(programObject, fragmentShaderObject);
+		glAttachShader(programId, vertexShaderId);
+		glAttachShader(programId, fragmentShaderId);
 		
-		glBindAttribLocation(programObject, 0, "vertices");
-		glBindAttribLocation(programObject, 1, "textures");
+		glBindAttribLocation(programId, 0, "vertices");
+		glBindAttribLocation(programId, 1, "textures");
 		
-		glLinkProgram(programObject);
-		if (glGetProgrami(programObject, GL_LINK_STATUS) != 1) {
-			LOG.error(glGetProgramInfoLog(programObject));
+		glLinkProgram(programId);
+		if (glGetProgrami(programId, GL_LINK_STATUS) != 1) {
+			LOG.error(glGetProgramInfoLog(programId));
 			throw new IllegalStateException();
 		}
-		glValidateProgram(programObject);
-		if (glGetProgrami(programObject, GL_VALIDATE_STATUS) != 1) {
-			LOG.error(glGetProgramInfoLog(programObject));
+		glValidateProgram(programId);
+		if (glGetProgrami(programId, GL_VALIDATE_STATUS) != 1) {
+			LOG.error(glGetProgramInfoLog(programId));
 			throw new IllegalStateException();
 		}
 	}
 	
 	public void setUniform(String uniformName, int value) {
-		int location = glGetUniformLocation(programObject, uniformName);
+		int location = glGetUniformLocation(programId, uniformName);
 		if (location != -1) glUniform1i(location, value);
 	}
 	
 	public void setUniform(String uniformName, Vector4f value) {
-		int location = glGetUniformLocation(programObject, uniformName);
+		int location = glGetUniformLocation(programId, uniformName);
 		if (location != -1) glUniform4f(location, value.x, value.y, value.z, value.w);
 	}
 	
 	public void setUniform(String uniformName, Matrix4f value) {
-		int location = glGetUniformLocation(programObject, uniformName);
+		int location = glGetUniformLocation(programId, uniformName);
 		FloatBuffer matrixData = BufferUtils.createFloatBuffer(16);
 		value.get(matrixData);
 		if (location != -1) glUniformMatrix4fv(location, false, matrixData);
 	}
 	
 	public void bind() {
-		glUseProgram(programObject);
+		glUseProgram(programId);
 	}
 	
 	private String readFile(String filename) {
@@ -97,10 +94,10 @@ public class Shader implements Disposable {
 
 	@Override
 	public void dispose() {
-		glDetachShader(programObject, vertexShaderObject);
-		glDetachShader(programObject, fragmentShaderObject);
-		glDeleteShader(vertexShaderObject);
-		glDeleteShader(fragmentShaderObject);
-		glDeleteProgram(programObject);
+		glDetachShader(programId, vertexShaderId);
+		glDetachShader(programId, fragmentShaderId);
+		glDeleteShader(vertexShaderId);
+		glDeleteShader(fragmentShaderId);
+		glDeleteProgram(programId);
 	}
 }
